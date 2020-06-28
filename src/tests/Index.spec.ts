d5935c1d1140e9ddd0975cf4c9820c66b66c930b
Marildo Cesar <marildocesar@hotmail.com>
2020-06-28 18:43:18 -0300

    Implementados testes

---
+++ src/tests/Index.spec.ts
@@ -0,0 +1,11 @@
+import UserController from "@controllers/UserController"
+
+test('it should be OK', () => {
+    const ctrl = new UserController()
+    const user = ctrl.getUser()
+    expect(user.name).toEqual('MCesar')
+})
+
+test('it should be fall', () => {
+    expect(2).toEqual(3)
+})
